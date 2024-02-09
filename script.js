e604caa4cca55e33a32ad4b16e6714986e0f2a84
AWebster7160 <156373395+AWebster7160@users.noreply.github.com>
2024-02-09 11:23:05 -0500

    Add and remove book function working

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const addBook = document.querySelector('dialog + button');
 const submitBook = document.querySelector('#submit');
 const bookDialog = document.querySelector('dialog');
 const bookForm = document.querySelector('form');
+const bookshelf = document.querySelector('.bookshelf');
 let inputTitle;
 let inputAuthor;
 let inputPages;
 let inputs = [];
-let bookNumber;
-let bookName = 'book';
+
 
 function Book(title, author, pages) {
     this.title = title;
@@ -30,18 +30,48 @@ function getInputs() {
     inputs.push(inputAuthor);
     inputs.push(inputPages);
     console.log(inputs);
-    var book1 = new Book(inputs[0], inputs[1], inputs[2]);
+    const book1 = new Book(inputs[0], inputs[1], inputs[2]);
+    console.log(book1);
     myLibrary.push(book1);
     console.log(myLibrary);
+    return myLibrary;
 }
 
-submitBook.addEventListener('click', (e) => {
-    e.preventDefault();
+function makeBook() {
+    let book = document.createElement('div');
+    book.style.display = 'revert';
+    let title = document.createElement('h3');
+    let author = document.createElement('h4');
+    let pages = document.createElement('span');
+    let deleteBtn = document.createElement('button');
+    book.setAttribute('class', 'book');
+    book.setAttribute('id', `${myLibrary.length-1}`)
+    title.setAttribute('class', 'title');
+    author.setAttribute('class', 'author');
+    pages.setAttribute('class', 'pages');
+    deleteBtn.setAttribute('class', 'delete-book')
+    bookshelf.appendChild(book);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(deleteBtn);
+    title.innerText = myLibrary[myLibrary.length-1].title;
+    author.innerText = myLibrary[myLibrary.length-1].author;
+    pages.innerText = myLibrary[myLibrary.length-1].pages;
+    deleteBtn.addEventListener('click', (event) => {
+    console.log(event.target.parentNode.id);
+    event.target.parentNode.style.display = 'none';
+    myLibrary.splice(`${(event.target.parentNode.id)}, 1`);
+    return myLibrary;
+})
+}
+
+submitBook.addEventListener('click', (event) => {
+    event.preventDefault();
     getInputs();
-    console.log(myLibrary)
+    makeBook();
+    bookDialog.close();
+    bookForm.reset();
 })
 
 
-function addBookToLibrary() {
-
-} 
